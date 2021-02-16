6acad8c3f3a9c6c3ae14ff33f227e6f3d0185d5b
Anfar-Asmaa <anfarasmaa@gmail.com>
2021-02-16 22:04:51 +0100

    ...

---
+++ src/main/java/com/ensias/test/JunitTesting.java
@@ -0,0 +1,5 @@
+package com.ensias.test;
+
+public class JunitTesting {
+
+}
---
+++ src/main/java/com/ensias/test/testing.java
@@ -0,0 +1,16 @@
+package com.ensias.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class testing {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
