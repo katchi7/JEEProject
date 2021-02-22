e12165437125bb29cd8a9eca17cba445cd12ea35
Anfar-Asmaa <anfarasmaa@gmail.com>
2021-02-22 19:30:39 +0100

    Unit testing on ModuleDao

--- src/main/java/com/ensias/test/ModuleDaoTest.java
+++ src/main/java/com/ensias/test/ModuleDaoTest.java
@@ -1,6 +1,7 @@
 package com.ensias.test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
@@ -32,53 +33,14 @@ import com.mysql.cj.Query;
 import com.ensias.beans.Module;
 import static org.mockito.Mockito.verify;
 
-
-
 class ModuleDaoTest {
 	
-	private static final String PROPERTIES_FILE = "/mysql.properties";
+	private static final String PROPERTIES_FILE = "/projectJee/resources/mysql.properties";
 	private static final String PROPERTY_URL ="url";
 	private static final String PROPERTY_DRIVER ="driver";
 	private static final String PROPERTY_USERNAME = "username";
     private static final String PROPERTY_PASSWORD    = "password";
-	 
-	 
-     
-    /* ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-     InputStream fichierProperties = classLoader.getResourceAsStream( PROPERTIES_FILE );
-     properties.load( fichierProperties );
-     String url1 = properties.getProperty( PROPERTY_URL );
-     String nomUtilisateur = properties.getProperty( PROPERTY_USERNAME );
-     String motDePasse = properties.getProperty( PROPERTY_PASSWORD );*/
-	
-
-	 Properties properties = new Properties();
-       String url;
-       String driver;
-       String nomUtilisateur;
-       String motDePasse;
-
-       ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-       InputStream fichierProperties = classLoader.getResourceAsStream( PROPERTIES_FILE );
-
-
-   
-          /* properties.load( fichierProperties );
-           url = properties.getProperty( PROPERTY_URL );
-           driver = properties.getProperty( PROPERTY_DRIVER );
-           nomUtilisateur = properties.getProperty( PROPERTY_USERNAME );
-           motDePasse = properties.getProperty( PROPERTY_PASSWORD );
-      
-
-      /* try {
-           Class.forName( driver );
-       } catch ( ClassNotFoundException e ) {
-           throw new DAOConfigurationException( "Le driver est introuvable dans le classpath.", e );
-       }*/
-       
-
-      instance = new DaoFactory( url, nomUtilisateur, motDePasse );
-
+	    
 	@Test
 	void testFindModuleById() {
 		ModuleDao module= Mockito.mock(ModuleDao.class);
@@ -88,29 +50,36 @@ class ModuleDaoTest {
         assertEquals(0,module.findModuleById(0).getElm_id());
 	}
 	
+	@Mock
+    private Connection conn; 
+	
+	@Mock
+    private PreparedStatement stm; 
+	
+	@Mock
+    private ResultSet dResult; 
+	
 	@Test
-	void testfindAllModules() throws SQLException, ClassNotFoundException {
-		Connection conn = DriverManager.getConnection(url,nomUtilisateur,motDePasse);
-		//Connection conn = null;
-		//conn =  factory.getConnection(); 
-		PreparedStatement stm = conn.prepareStatement("SELECT * FROM element;");
+	void testfindAllModules() throws SQLException, ClassNotFoundException, IOException {
+
+    //DaoFactory instance = new DaoFactory( url, nomUtilisateur, motDePasse );
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//Connection conn = Mockito.mock(Connection.class);
+		//PreparedStatement stm= Mockito.mock(PreparedStatement.class);
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ensias_doc","root","bac2017bac.");
+		stm = conn.prepareStatement("SELECT * FROM element;");
 		
 		ModuleDao module= Mockito.mock(ModuleDao.class);
-		//Query query = Mockito.mock(Query.class);
-        when(conn.prepareStatement("SELECT * FROM element;")).thenReturn(stm);
-        // stub the query returned above to return a meaningful result when somebody
-        // asks for the result list
-        ResultSet dResult = (ResultSet) new LinkedList<Module>();
+       	
         when(stm.executeQuery()).thenReturn(dResult);
-
-        // let's call findAll() and see what it does
-        List<Module> result = module.findAllModules();
-
-        // did it request the named query?
-        verify(conn).prepareStatement("SELECT * FROM element;");
-        // did it ask for the result list of the named query?
-        verify(stm).executeQuery();
-        // did it return the result list of the named query?
+       
+        ArrayList<Module> result = module.findAllModules();
+        result= null;
+        
+        //verify(conn).prepareStatement("SELECT * FROM element;");
+       
+        //verify(stm).executeQuery();
+       
         assertSame(dResult, result);
 	}
 }
